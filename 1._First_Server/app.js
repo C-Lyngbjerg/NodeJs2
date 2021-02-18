76bd50e1a4d797279b7a37655cf15ff36b6e3f5d
C-Lyngbjerg <56966598+C-Lyngbjerg@users.noreply.github.com>
2021-02-18 14:30:59 +0100

    1._first_server done and 2._second_server started

--- 1._First_Server/app.js
+++ 1._First_Server/app.js
@@ -1,19 +1,55 @@
 const express = require("express");
 const app = express();
-const user = {
-    name: "Christoffer",
-    grade: 12,
-    course: "NodeJs"
-}
+const students = 
+    [{
+        id: 1,
+        name: "Christoffer",
+        grade: 07,
+        course: "NodeJs"
+    },
+    {
+        id: 2,
+        name: "Cecilie",
+        grade: 12,
+        course: "NodeJs"
+    },
+    {
+        id: 3,
+        name: "Jakob",
+        grade: 12,
+        course: "CreativeCode"
+    },
+    {
+        id: 4,
+        name: "Sebastian",
+        grade: 10,
+        course: "NodeJs"
+    }]
+    const greeting = {value:"Greetings, please go to /students or /students/<id>"}
 
 // find a noun - something to CRUD - what kind of datatype to store in
 // implement get and get by id - id can be hardcoded if needed
-app.get("/", (req, res) =>{
-    res.send({message: "First call to /"});
+app.get("/", (req, res) => {
+    res.send(greeting);
 });
 
-app.get("/anotherpath", (req, res) => {
-    res.send({message: "another thing"})
+app.get("/students", (req, res) => {
+    res.send(students)
+});
+
+app.get("/students/:id", (req, res) => {
+    let studentsList = students.filter((s) => s.id == req.params.id)
+    res.send(studentsList);
 });
 
 app.listen(8080);
+
+// (method) Array<{ id: number; name: string; grade: number; course: string; }>
+// .map<string>(callbackfn: (value: { id: number; name: string;  grade: number; course: string;}, 
+//     index: number, 
+//     array: {id: number; name: string; grade: number; course: string; }[]) => string, thisArg?: any): string[]
+// Calls a defined callback function on each element of an array, and returns an array that contains the results.
+
+// @param callbackfn — A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+
+// @param thisArg — An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
